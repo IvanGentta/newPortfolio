d88bfb6b71f4353f20a6f6da30ea7c41b4097f91
IvanGentta <genttaivan@gmail.com>
2025-01-13 18:50:37 -0300

    apostrophe fix

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,11 +5,11 @@ export default function About() {
   return (
     <div className="flex">
       <h2>
-        I started studying on my own out of pure curiosity, and since then, I've
-        been captivated by the world of programming. I love that it's a
-        constantly evolving field, which motivates me to keep growing and
-        developing as a professional. Currently, I'm pursuing a degree in
-        Computer Programming at UNSAM. Take a look around!
+        I started studying on my own out of pure curiosity, and since then,
+        I&apos;ve been captivated by the world of programming. I love that
+        it&apos;s a constantly evolving field, which motivates me to keep
+        growing and developing as a professional. Currently, I&apos;m pursuing a
+        degree in Computer Programming at UNSAM. Take a look around!
       </h2>
       <Image
         src="/images/fotoCarnet1(730px).jpeg"
