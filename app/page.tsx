7146b211d54fc20a0992d9925d17de9e3dcf5818
IvanGentta <genttaivan@gmail.com>
2025-09-17 20:34:37 -0300

    Button github linkedin

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ import Skills from "./skills/page";
 import { useTypeHook } from "./typeHook";
 import { useLanguage } from "./LanguageContext";
 import { translations } from "./translations";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import Link from "next/link";
 
 export default function Home() {
   const { language } = useLanguage();
@@ -22,7 +24,7 @@ export default function Home() {
   );
 
   return (
-    <main className="w-full h-auto bg-iviBackground text-iviText">
+    <main className="w-full h-auto bg-iviBackground text-iviText ">
       <div className="min-h-screen flex flex-col items-center justify-center text-center mx-1">
         <p className="flex items-center h-7 relative font-mono">
           {text}
@@ -41,6 +43,24 @@ export default function Home() {
           </span>{" "}
           {translations[language].portfolio}
         </h1>
+        <div className="flex space-x-4 pt-4 z-40">
+          <div className="cursor-pointer hover:scale-110 transition ease-in-out">
+            <Link href="https://github.com/IvanGentta" target="_blank">
+              <FaGithub size={50} />
+            </Link>
+          </div>
+          <div>
+            <Link
+              href="https://www.linkedin.com/in/iv%C3%A1n-ariel-gentta-117077231/"
+              target="_blank"
+            >
+              <FaLinkedinIn
+                size={50}
+                className="cursor-pointer hover:scale-110 transition ease-in-out"
+              />
+            </Link>
+          </div>
+        </div>
       </div>
       <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-iviText via-transparent to-transparent opacity-50 rounded-3xl"></div>
       <div className="flex flex-col items-center w-full">
